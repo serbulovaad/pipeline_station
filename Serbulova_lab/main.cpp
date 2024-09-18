ed2e0815069cffb4508a6177a64ea3614084419a
crazzyfuncat <s.alla.d@yandex.ru>
2024-09-19 01:06:20 +0300

    did a menu and try to do a check didnt work)

--- Serbulova_lab/main.cpp
+++ Serbulova_lab/main.cpp
@@ -2,30 +2,133 @@
 #include <string>
 using namespace std;
 
+
+
 //properties
 struct pipe
 {
 	int name;
-	int length;
-	float diameter;
-	bool status_repair; // 0 False - ready for use, 1 True - under repair
+	int l; // length;
+	float d; // diameter;
+	bool repair; // 0 False - ready for use, 1 True - under repair
 };
 
 struct CS //Compressor station
 {
 	string name;
-	int number_ws; // workstations in use
-	int efficienty;
+	int ws; // workstations in use
+	string eff; //efficienty;
 
 };
 
+void inputCheck(int& x) // ввод с проверкой, он нужен?
+{
+	while (1)
+	{
+		if (cin)
+		{
+			cin >> x;
+			break;
+		}
+		else
+		{
+			cin.clear();
+			cin.ignore(1000, '\n');
+		}
+	}
+}
+ostream& operator << (ostream& out, const pipe& p)
+{
+	out << "name: " << p.name << endl
+		<< "d = " << p.d << endl
+		<< "l = " << p.l << endl
+		<< "repair = " << p.repair << endl;
+
+	return out;
+}
+
+istream& operator >> (istream& in, pipe& p) // как тут сделать проверку?
+{
+	cout << "name: ";
+	in >> p.name;
+	cout << "d = ";
+	in >> p.d;
+	cout << "l = ";
+	in >> p.l;
+	cout << "repair = ";
+	in >> p.repair;
+
+	return in;
+}
+
 int MenuOutput()
 {
-	cout << "Меню\nВыберите опцию\n"
-		"1. Добавить трубу\n2. Добавить КС\n3. Просмотр всех объектов"
-		"\n4. Редактировать трубу\n5. Редактировать КС\n"
-		"6. Сохранить\n7. Загрузить\n"
-		"0. Выход";
+	while (1)
+	{
+
+		cout << "Меню\nВыберите опцию\n"
+			"1. Добавить трубу\n2. Добавить КС\n3. Просмотр всех объектов"
+			"\n4. Редактировать трубу\n5. Редактировать КС\n"
+			"6. Сохранить\n7. Загрузить\n"
+			"0. Выход\n" << endl;
+
+		int option;
+		cout << "Выберите опцию: ";
+		inputCheck(option);
+		cout << endl;
+		
+		if (option != 0)
+		{
+			switch (option)
+			{
+				case 1:
+					cout << "Добавьте трубу" << endl;
+
+					cout << endl;
+					break;
+				case 2:
+					cout << "Добавьте КС" << endl;
+
+					cout << endl;
+					break;
+				case 3:
+					cout << "Просмотр всех объектов" << endl;
+
+					cout << endl;
+					break;
+				case 4:
+					cout << "Редактирование трубы" << endl;
+
+					cout << endl;
+					break;
+				case 5:
+					cout << "Редактирование КС" << endl;
+
+					cout << endl;
+					break;
+				case 6:
+					cout << "Сохранение" << endl;
+
+					cout << endl;
+					break;
+				case 7:
+					cout << "Выход" << endl;
+
+					cout << endl;
+					break;
+				default:
+					cout << "Ошибка, введено неверное число" << option << endl
+						<< "Повторите попытку" << endl << endl;
+					break;
+			}
+		} 
+		else 
+		{
+			cout << "До свидания!" << endl;
+			return 0;
+		}
+		
+	}
 }
 
 int main()
@@ -34,12 +137,7 @@ int main()
 
 	cout << "Привет Hello" << endl;
 
-	while (true)
-	{
-		int option = MenuOutput;
-		if (option == 0)
-			cout << "До свидания!";
-	}
+	MenuOutput();
 
 
 	return 0;
