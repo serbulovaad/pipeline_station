ade0161db63920779ab8fa57ccf79693579cd01b
crazzyfuncat <s.alla.d@yandex.ru>
2024-09-19 13:33:56 +0300

    check done

--- Serbulova_lab/main.cpp
+++ Serbulova_lab/main.cpp
@@ -7,37 +7,35 @@ using namespace std;
 //properties
 struct pipe
 {
-	int name;
+	string name = "";
 	int l; // length;
-	float d; // diameter;
+	double d; // diameter;
 	bool repair; // 0 False - ready for use, 1 True - under repair
 };
 
 struct CS //Compressor station
 {
-	string name;
+	string name = "";
 	int ws; // workstations in use
-	string eff; //efficienty;
+	double eff; //efficienty;
 
 };
 
-void inputCheck(int& x) // ввод с проверкой, он нужен?
+template <typename T>
+T inputCheck(istream& in = cin)
 {
-	while (1)
+	T x;
+	while ((in >> x).fail()	// check type
+		|| in.peek() != '\n')	// is buffer empty (int/float check)
 	{
-		if (cin)
-		{
-			cin >> x;
-			break;
-		}
-		else
-		{
-			cin.clear();
-			cin.ignore(1000, '\n');
-		}
+		in.clear();
+		in.ignore(10000, '\n');
+		cout << "Try again\n ";
 	}
+	return x;
 }
-ostream& operator << (ostream& out, const pipe& p)
+
+ostream& operator << (ostream& out, const pipe& p) //
 {
 	out << "name: " << p.name << endl
 		<< "d = " << p.d << endl
@@ -47,84 +45,160 @@ ostream& operator << (ostream& out, const pipe& p)
 	return out;
 }
 
+ostream& operator << (ostream& out, const CS& cs)
+{
+	out << "name: " << cs.name << endl
+		<< "ws = " << cs.ws << endl
+		<< "eff = " << cs.eff << endl;
+
+	return out;
+}
+
 istream& operator >> (istream& in, pipe& p) // как тут сделать проверку?
 {
-	cout << "name: ";
-	in >> p.name;
-	cout << "d = ";
-	in >> p.d;
-	cout << "l = ";
-	in >> p.l;
-	cout << "repair = ";
+	cout << "name (str): ";
+	p.name = inputCheck<string>(in);
+	cout << "d (int) = ";
+	p.d = inputCheck<int>(in);
+	cout << "l (double) = ";
+	p.l = inputCheck<double>(in);
+	cout << "repair (true - under repair, false - ready for use) =";
 	in >> p.repair;
 
 	return in;
 }
 
+istream& operator >> (istream& in, CS& cs) // как тут сделать проверку?
+{
+	cout << "name (str): ";
+	in >> cs.name;
+	cout << "ws (int) = ";
+	in >> cs.ws;
+	cout << "eff (double) = ";
+	in >> cs.eff;
+	
+	return in;
+}
+
+int addPipe(pipe& p)
+{
+	//if (p.name != "")
+	//{
+		cout << "Add pipe" << endl;
+		cin >> p;
+		cout << endl;
+	//}
+	//else
+	//	cout << "Pipe already exist\n" << endl;
+
+	return 0;
+}
+
+int addCS(CS& cs)
+{
+	if (cs.name != "")
+	{
+		cout << "Add CS" << endl;
+		cin >> cs;
+		cout << endl;
+	}
+	else
+		cout << "CS already exist\n" << endl;
+	return 0;
+}
+
+void viewAll(const pipe& p, const CS& cs)
+{
+	cout << "All objects\n\n";
+	if (p.name != "")
+		cout << "pipe:\n" << p << endl;
+	else
+		cout << "Pipe is not found\n" << endl;
+	if (cs.name != "")
+		cout << "CS:\n" << cs << endl;
+	else
+		cout << "CS is not found\n" << endl;
+}
+
+int editPipe(pipe& p)
+{
+
+	return 0;
+}
+
+int editCS(CS& cs)
+{
+
+	return 0;
+}
+
+
 int MenuOutput()
 {
+	pipe p;
+	CS cs;
+
 	while (1)
 	{
 
-		cout << "Меню\nВыберите опцию\n"
-			"1. Добавить трубу\n2. Добавить КС\n3. Просмотр всех объектов"
-			"\n4. Редактировать трубу\n5. Редактировать КС\n"
-			"6. Сохранить\n7. Загрузить\n"
-			"0. Выход\n" << endl;
+		cout << "Menu\n\n"
+			"1. Add pipe\n2. Add CS\n3. View all objects"
+			"\n4. Edit pipe\n5. Edit CS\n"
+			"6. Save\n7. Download\n"
+			"0. Exit\n" << endl;
 
 		int option;
-		cout << "Выберите опцию: ";
-		inputCheck(option);
+		cout << "Choose option: ";
+		option = inputCheck<int>();
 		cout << endl;
-		
+
 		if (option != 0)
 		{
 			switch (option)
 			{
 				case 1:
-					cout << "Добавьте трубу" << endl;
-
-					cout << endl;
+				{
+					addPipe(p);
 					break;
+				}
+					
 				case 2:
-					cout << "Добавьте КС" << endl;
-
-					cout << endl;
+				{
+					addCS(cs);
 					break;
+				}
+					
 				case 3:
-					cout << "Просмотр всех объектов" << endl;
-
-					cout << endl;
+				{
+					viewAll(p, cs);
 					break;
+				}
+					
 				case 4:
-					cout << "Редактирование трубы" << endl;
-
-					cout << endl;
+				{
+					editPipe(p);
 					break;
+				}
 				case 5:
-					cout << "Редактирование КС" << endl;
-
-					cout << endl;
+				{
+					editCS(cs);
 					break;
+				}
 				case 6:
-					cout << "Сохранение" << endl;
 
-					cout << endl;
 					break;
 				case 7:
-					cout << "Выход" << endl;
 
-					cout << endl;
 					break;
 				default:
-					cout << "Ошибка, введено неверное число" << option << endl
-						<< "Повторите попытку" << endl << endl;
+					cout << "Error! Wrong number" << option << endl
+						<< "Try again" << endl << endl;
 					break;
 			}
 		} 
 		else 
 		{
-			cout << "До свидания!" << endl;
+			cout << "Goodbye!" << endl;
 			return 0;
 		}
 		
